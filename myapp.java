4d22064342d9de2a71fdb3f0da35952cef292d9a
Theertha12345 <112471686+Theertha12345@users.noreply.github.com>
2025-12-26 13:53:20 +0530

    Create myapp.java with main and add methods

---
+++ myapp.java
@@ -0,0 +1,11 @@
+package com.dev;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Developer Maven app build successful");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
